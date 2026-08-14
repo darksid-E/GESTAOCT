7bc60b0705f5d4f5dfeb3b8aebe275851af57902
Felipe Torres  Vieira     [PROV] <ftorre@ternium.com.br>
2026-08-14 11:46:09 -0300

    Correção da cor do top temp

--- script.js
+++ script.js
@@ -1750,7 +1750,7 @@ document.addEventListener("DOMContentLoaded", () => {
             data: {
                 labels,
                 datasets: [
-                    { label: 'TOPO TEMP.', data: registros.map(r => r.topo), borderColor: 'lightgray', backgroundColor: 'lightgray', yAxisID: 'yTemp', tension: 0.15, pointRadius: 0, borderWidth: 1.6, spanGaps: true },
+                    { label: 'TOPO TEMP.', data: registros.map(r => r.topo), borderColor: 'black', backgroundColor: 'black', yAxisID: 'yTemp', tension: 0.15, pointRadius: 0, borderWidth: 1.6, spanGaps: true },
                     { label: 'LC TEMP.', data: registros.map(r => r.lc), borderColor: 'darkblue', backgroundColor: 'darkblue', yAxisID: 'yTemp', tension: 0.15, pointRadius: 0, borderWidth: 1.6, spanGaps: true },
                     { label: 'LM TEMP.', data: registros.map(r => r.lm), borderColor: 'red', backgroundColor: 'red', yAxisID: 'yTemp', tension: 0.15, pointRadius: 0, borderWidth: 1.6, spanGaps: true },
                     { label: 'CK TIME', data: registros.map(r => r.ck), borderColor: 'green', backgroundColor: 'green', yAxisID: 'yTemp', tension: 0.15, pointRadius: 0, borderWidth: 1.6, borderDash: [4, 3], spanGaps: true },
